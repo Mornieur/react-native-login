02757a3a9e2f0fc8dd361c24a9f3c29c9e1adb28
Mornieur <mariafernanda-coro@hotmail.com>
2023-05-23 14:59:30 -0300

    build: 📕 add storybook settings

---
+++ .storybook/main.js
@@ -0,0 +1,15 @@
+module.exports = {
+  stories: ['../components/**/*.stories.?(ts|tsx|js|jsx)'],
+  addons: [
+    '@storybook/addon-ondevice-controls',
+    '@storybook/addon-ondevice-actions',
+    '@storybook/addon-react-native-web',
+    'storybook-addon-designs',
+    '@storybook/addon-essentials',
+    '@storybook/addon-links',
+    '@storybook/addon-interactions',
+    '@storybook/addon-storysource',
+    '@storybook/addon-jest',
+    'storybook-addon-designs',
+  ],
+};
---
+++ .storybook/theme.js
@@ -0,0 +1,7 @@
+import { create } from '@storybook/theming';
+
+export default create({
+  colorSecondary: '#5D5DF9',
+  base: '#333',
+  brandTitle: 'Libellus - Storybook',
+});
