1560f2f34c73d4a9a2748b4d2866f503ff122969
Mornieur <mariafernanda-coro@hotmail.com>
2023-05-23 15:07:07 -0300

    style: 🖌️ create a global style for storybook components

---
+++ src/stories/styles/index.ts
@@ -0,0 +1,18 @@
+import styled from 'styled-components'
+
+export const Container = styled.div`
+  width: 100vw;
+  height: 100vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`
+export const SimpleContainer = styled.div`
+  height: 25px;
+  display: flex;
+  align-items: center;
+
+  p {
+    font-weight: 600;
+  }
+`
